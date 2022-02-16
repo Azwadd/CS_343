b17cc916590baa5f9cdb1cb92a824edc73b9aad6
azwad <shameemazwad@gmail.com>
2022-02-15 19:28:07 -0500

    Update

--- Labs/Lab 1/Shameem_Azwad__Feb_13_2022_mux_2_1/Shameem_Azwad_mux_2_1.vhd
+++ Labs/Lab 1/Shameem_Azwad__Feb_13_2022_mux_2_1/Shameem_Azwad_mux_2_1.vhd
@@ -2,11 +2,11 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 
 ENTITY Shameem_Azwad_mux_2_1 IS
-	PORT (A, B, X : IN STD_LOGIC;
+	PORT (X, A, B : IN STD_LOGIC;
 		   Y: OUT STD_LOGIC);
 END Shameem_Azwad_mux_2_1;
 
 ARCHITECTURE LogicFunction OF Shameem_Azwad_mux_2_1 IS
 BEGIN
-	Y <= (A AND NOT X) OR (B AND X);
+	Y <= ((A AND NOT X) OR (B AND X));
 END LogicFunction ;
